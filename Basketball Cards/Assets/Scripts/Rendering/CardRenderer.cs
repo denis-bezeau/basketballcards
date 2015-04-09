b66c9979520b7673206b10af033f3bc950806793
surrealarboreal <surrealarboreal@gmail.com>
2015-04-08 22:40:10 -0300

    A card prefab and all the jazz that comes with it. * Some Project Directory work, including some resources folder that I realized doesn't actually work in Unity5, but I decided the name was fine for organization anyway. * OpenSans font for the card typeset. * Some basic card Materials * A great gloss shader that uses RGB values instead of extra art assets to provide a fairly accurate "glossy" look, using unitys built in shaders seems to work fine for now. * LEBRON JAMES * Z Depth font shader to allow use of 3d text that adheres to the depth buffer.

---
+++ Basketball Cards/Assets/Scripts/Rendering/CardRenderer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Card renderer.
+/// </summary>
+public class CardRenderer : MonoBehaviour 
+{
+	[SerializeField]
+	private TextField[] textFields;
+
+	[SerializeField]
+	private SpriteRenderer cardSprite;
+
+	public void Init()
+	{
+
+	}
+}
+
+[Serializable]
+public class TextField
+{
+	[SerializeField]
+	private eCardTextFieldType textFieldType;
+	[SerializeField]
+	private TextMesh textMesh;
+}
+
+public enum eCardTextFieldType
+{
+	Title = 0,
+	Power,
+	Defense,
+	Description
+}
